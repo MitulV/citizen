9e98b61e0cf0535bc0866af3c86ecf71b7622f03
Mitul <mitulvpatel2211@gmail.com>
2024-06-06 15:31:48 +0530

    result complet

--- resources/js/Pages/TestResult.jsx
+++ resources/js/Pages/TestResult.jsx
@@ -9,10 +9,7 @@ import {
     faRedo,
     faChevronLeft,
 } from "@fortawesome/free-solid-svg-icons";
-import {
-    CircularProgressbar,
-    buildStyles
-} from "react-circular-progressbar";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
 export default function TestStart() {
@@ -20,23 +17,39 @@ export default function TestStart() {
         <>
             <Head title="Premium" />
             <Header />
-            <section className="min-h-screen grid grid-cols-1 lg:grid-cols-12 gap-8 p-4 md:p-10 bg-slate-50">
+            <section className="min-h-screen grid grid-cols-1 lg:grid-cols-12 gap-10 p-4 md:p-10 bg-slate-50">
                 {/* Left Side (spans 12 columns on small screens, 4 columns on large screens) */}
-                <div className="order-2 lg:order-1 lg:col-span-4 flex flex-col space-y-4 border rounded-xl bg-white p-6 md:p-10">
-                    <h1 className="text-xl sm:text-2xl lg:text-3xl font-bold">How to Pass the first time</h1>
+                <div className="order-2 lg:order-1 lg:col-span-4 flex flex-col space-y-4 rounded-xl bg-white p-6 md:p-10">
+                    <h1 className="text-xl sm:text-2xl lg:text-3xl font-bold">
+                        How to Pass the First Time
+                    </h1>
                     <ul className="space-y-2 text-gray-600 mt-4">
                         <li className="flex items-center">
-                            <FontAwesomeIcon icon={faCheck} className="mr-2 text-green-500" />
-                            Access to Over 600+ Test Questions                        </li>
+                            <FontAwesomeIcon
+                                icon={faCheck}
+                                className="mr-2 text-gray-600"
+                            />
+                            Access to Over 600+ Test Questions
+                        </li>
                         <li className="flex items-center">
-                            <FontAwesomeIcon icon={faCheck} className="mr-2 text-green-500" />
+                            <FontAwesomeIcon
+                                icon={faCheck}
+                                className="mr-2 text-gray-600"
+                            />
                             Practice tests for each chapter
                         </li>
                         <li className="flex items-center">
-                            <FontAwesomeIcon icon={faCheck} className="mr-2 text-green-500" />
-                            Unlimited exam simulation runs                        </li>
+                            <FontAwesomeIcon
+                                icon={faCheck}
+                                className="mr-2 text-gray-600"
+                            />
+                            Unlimited exam simulation runs
+                        </li>
                         <li className="flex items-center">
-                            <FontAwesomeIcon icon={faCheck} className="mr-2 text-green-500" />
+                            <FontAwesomeIcon
+                                icon={faCheck}
+                                className="mr-2 text-gray-600"
+                            />
                             Tailored study plan
                         </li>
                     </ul>
@@ -44,25 +57,93 @@ export default function TestStart() {
                         <img
                             src="/images/testresult5.png"
                             alt="Placeholder"
-                            className="w-60 h-80 gap-0 rounded-tl-[24px] rounded-tr-none rounded-br-none rounded-bl-none"
-                        />                        <button className="bg-primary text-white px-6 py-2 rounded-full ">Pass with premium guaranteed </button>
-                        <h2 className="text-base  mt-4">100% Money Back Guarantee</h2>
+                            className="w-60 h-80 rounded-tl-[24px] rounded-tr-none rounded-br-none rounded-bl-none"
+                        />
+                        <button className="bg-primary text-white px-6 py-2 rounded-full mt-8">
+                            Pass with Premium Guaranteed
+                        </button>
+                        <h2 className="text-base mt-4">100% Money Back Guarantee</h2>
                     </div>
                 </div>
 
                 {/* Right Side (spans 12 columns on small screens, 8 columns on large screens) */}
-                <div className="order-1 md:h-64 lg:order-2 lg:col-span-8 flex flex-col space-y-4 border rounded-xl bg-white p-6 md:p-10">
-                    <div className="flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4">
+                <div className="order-1 lg:order-2 lg:col-span-8 flex flex-col space-y-10 md:space-y-4 rounded-2xl bg-white p-6 md:p-10 h-64">
+                    {/* First Section on Right */}
+                    <div className="flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4 ">
                         {/* Progress circle */}
-                        <CircularProgressbar value={85.71} text={`${85.71}%`} strokeWidth={5} 
-                        styles={buildStyles({
-                            textColor: "black",
-                            pathColor: "green",
-                        })} />;
-                        <div className="flex flex-col">
-                            <h1 className="text-xl font-bold">Great. You passed!</h1>
-                            <p className="text-gray-600">There are more Practice questions left to go , Unlock 600+ practice questions today</p>
-                            <button className="bg-primary text-white mt-4 px-6 py-2 rounded-full">Review</button>
+                        <div className="w-40 h-40">
+                            <CircularProgressbar
+                                value={85.71}
+                                text={`${85.71}%`}
+                                strokeWidth={8}
+                                styles={buildStyles({
+                                    textColor: "black",
+                                    pathColor: "#3E9330",
+                                })}
+                            />
+                        </div>
+                        <div className="flex flex-col ">
+                            <h1 className="text-3xl font-bold text-lime-700 p-2 ml-10">
+                                Great. You Passed!
+                            </h1>
+                            <p className="text-zinc-600 p-2 ml-10">
+                                There are more practice questions left to go,
+                                unlock 600+ practice questions today.
+                            </p>
+                            <div className="flex items-center space-x-4 p-2 ml-10">
+                                <div className="w-2 h-2 bg-green-500 rounded-full"></div>
+                                <span className="text-zinc-500">12 Correct</span>
+                                <div className="w-2 h-2 bg-red-500 rounded-full"></div>
+                                <span className="text-zinc-500">2 Wrong</span>
+                                <FontAwesomeIcon
+                                    icon={faClock}
+                                    className="text-zinc-500"
+                                />
+                                <span className="text-zinc-500">
+                                    2 minutes 2 seconds
+                                </span>
+                            </div>
+                            <button className="bg-primary text-white px-6 py-2 rounded-full mt-4 w-full sm:w-auto ml-10">
+                                Next up, Practice Test 2
+                            </button>
+                        </div>
+                    </div>
+
+                    {/* Second Section on Right */}
+                    
+                    <div className="flex flex-col space-y-4 ">
+                        <h1 className="text-xl sm:text-2xl font-bold mt-12">
+                            What’s next?
+                        </h1>
+                        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
+                            {/* Card 1 */}
+                            <div className="border p-6 rounded-2xl bg-white">
+                                <h1 className="text-xl font-bold">Restart this test</h1>
+                                <p>Want to revisit the same questions you just did? Take this test once again.</p>
+                                <h2 className="text-primary underline underline-offset-1">Restart</h2>
+                            </div>
+                            {/* Card 2 */}
+                            <div className="border p-6 rounded-2xl bg-white">
+                                <h1 className="text-xl font-bold">Continue to the next test</h1>
+                                <p className="mt-2">Keep practicing with a new set of questions for the Canadian Citizenship Test.
+                                </p>
+                                <h2 className="text-primary underline underline-offset-1">Restart</h2>
+                            </div>
+                            {/* Card 3 */}
+                            <div className="border p-6 rounded-2xl bg-white">
+                                <h1 className="text-xl font-bold">Review your answers</h1>
+                                <p className="mt-2">See which answers you chose and what the correct answers are.
+                                </p>
+                                <h2 className="text-primary underline underline-offset-1">Restart</h2>
+                            </div>
+                            {/* Card 4 */}
+                            <div className="border p-6 rounded-2xl bg-indigo-600 text-white">
+                                <h1 className="text-xl font-bold">Restart this test</h1>
+                                <p className="mt-2">Want to revisit the same questions you just did? Take this test once again.</p>
+                                <button className="bg-primary text-white px-24 py-2 rounded-full mt-4 w-full sm:w-auto ">
+                                Unlock all 600+ Questions
+                            </button>
+                            </div>
                         </div>
                     </div>
                 </div>
