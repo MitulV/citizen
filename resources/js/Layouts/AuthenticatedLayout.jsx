2beafefef9c1ec0fb7d5be7606f665bc2088dcc9
Mitul <mitulvpatel2211@gmail.com>
2024-06-22 16:04:22 +0530

    changes

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -10,15 +10,10 @@ import {
     faFileLines,
     faImage,
     faGem,
-} from "@fortawesome/free-solid-svg-icons";
-import { Accordion } from "flowbite-react";
-import {
     faCheck,
     faTimes,
-    faClock,
-    faRedo,
-    faChevronLeft,
 } from "@fortawesome/free-solid-svg-icons";
+import { Accordion } from "flowbite-react";
 export default function Authenticated({
     user,
     children,
@@ -27,27 +22,9 @@ export default function Authenticated({
     topicId,
 }) {
     const [collapsed, setCollapsed] = useState(false);
-    const [dropdownOpen1, setDropdownOpen1] = useState(false); // State for first dropdown
-    const [dropdownOpen2, setDropdownOpen2] = useState(false); // State for second dropdown
 
     const toggleSidebar = () => {
         setCollapsed(!collapsed);
-    };
-
-    const toggleDropdown1 = () => {
-        setDropdownOpen1(!dropdownOpen1);
-    };
-
-    const toggleDropdown2 = () => {
-        setDropdownOpen2(!dropdownOpen2);
-    };
-
-    const closeDropdown1 = () => {
-        setDropdownOpen1(false);
-    };
-
-    const closeDropdown2 = () => {
-        setDropdownOpen2(false);
     };
 
     useEffect(() => {
@@ -64,7 +41,7 @@ export default function Authenticated({
             <div className="relative flex h-full">
                 <Sidebar
                     collapsedWidth="0"
-                    width="177px"
+                    width="162px"
                     collapsed={collapsed}
                     backgroundColor="rgb(254,117,62)"
                 >
@@ -167,7 +144,7 @@ export default function Authenticated({
                                     <Accordion.Title>Chapter 1</Accordion.Title>
                                     <Accordion.Content>
                                         <div className="text-gray-800 text-start">
-                                            <div className="p-4 bg-slate-50 ">
+                                            <div className="p-4 bg-slate-50 flex justify-between items-start">
                                                 <button className=" text-base ">
                                                     The Oath of Citizenship
                                                 </button>
@@ -176,7 +153,7 @@ export default function Authenticated({
                                                     className="text-green-500 "
                                                 />
                                             </div>
-                                            <div className="p-4 bg-slate-50 mt-1 ">
+                                            <div className="p-4 bg-slate-50 mt-1 flex justify-between items-start">
                                                 <button className="bg-slate-50  text-base text-gray-800 text-start">
                                                     A message to the new citizen
                                                 </button>
@@ -185,91 +162,7 @@ export default function Authenticated({
                                                     className="text-red-500 "
                                                 />
                                             </div>
-                                            <div className="p-4 bg-slate-50 mt-1">
-                                                <button className="bg-slate-50  text-base text-gray-800 text-start">
-                                                    About the citizen
-                                                </button>
-                                            </div>
-                                            <div className="p-4 bg-slate-50 mt-1 ">
-                                                <button className="bg-slate-50 text-base text-gray-800 text-start">
-                                                    Transition to the crown
-                                                </button>
-                                            </div>
                                         </div>
-                                    </Accordion.Content>
-                                </Accordion.Panel>
-                                <Accordion.Panel>
-                                    <Accordion.Title>
-                                        Is there a Figma file available?
-                                    </Accordion.Title>
-                                    <Accordion.Content>
-                                        <p className="mb-2 text-gray-500 dark:text-gray-400">
-                                            Flowbite is first conceptualized and
-                                            designed using the Figma software so
-                                            everything you see in the library
-                                            has a design equivalent in our Figma
-                                            file.
-                                        </p>
-                                        <p className="text-gray-500 dark:text-gray-400">
-                                            Check out the
-                                            <a
-                                                href="https://flowbite.com/figma/"
-                                                className="text-cyan-600 hover:underline dark:text-cyan-500"
-                                            >
-                                                Figma design system
-                                            </a>
-                                            based on the utility classes from
-                                            Tailwind CSS and components from
-                                            Flowbite.
-                                        </p>
-                                    </Accordion.Content>
-                                </Accordion.Panel>
-                                <Accordion.Panel>
-                                    <Accordion.Title>
-                                        What are the differences between
-                                        Flowbite and Tailwind UI?
-                                    </Accordion.Title>
-                                    <Accordion.Content>
-                                        <p className="mb-2 text-gray-500 dark:text-gray-400">
-                                            The main difference is that the core
-                                            components from Flowbite are open
-                                            source under the MIT license,
-                                            whereas Tailwind UI is a paid
-                                            product. Another difference is that
-                                            Flowbite relies on smaller and
-                                            standalone components, whereas
-                                            Tailwind UI offers sections of
-                                            pages.
-                                        </p>
-                                        <p className="mb-2 text-gray-500 dark:text-gray-400">
-                                            However, we actually recommend using
-                                            both Flowbite, Flowbite Pro, and
-                                            even Tailwind UI as there is no
-                                            technical reason stopping you from
-                                            using the best of two worlds.
-                                        </p>
-                                        <p className="mb-2 text-gray-500 dark:text-gray-400">
-                                            Learn more about these technologies:
-                                        </p>
-                                        <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
-                                            <li>
-                                                <a
-                                                    href="https://flowbite.com/pro/"
-                                                    className="text-cyan-600 hover:underline dark:text-cyan-500"
-                                                >
-                                                    Flowbite Pro
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a
-                                                    href="https://tailwindui.com/"
-                                                    rel="nofollow"
-                                                    className="text-cyan-600 hover:underline dark:text-cyan-500"
-                                                >
-                                                    Tailwind UI
-                                                </a>
-                                            </li>
-                                        </ul>
                                     </Accordion.Content>
                                 </Accordion.Panel>
                             </Accordion>
@@ -284,7 +177,7 @@ export default function Authenticated({
                          collapsed
                              ? "left-0 ml-0"
                              : isChapterPanelVisible
-                             ? "left-64  ml-44"
+                             ? "left-64  ml-64"
                              : "left-44 ml-0"
                      } 
                       transform rotate-180 p-1 w-8 h-8 bg-white rounded-l-full transition-all duration-300 border border-gray-200 shadow-sm hover:shadow-lg`}
