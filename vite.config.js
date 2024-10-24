bee36326fe6a65c02c19ed00330b9d3324dc1801
Mitul <mitulvpatel2211@gmail.com>
2024-10-24 21:41:59 +0530

    Vite config changes for fonts warning

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,20 @@ export default defineConfig({
         }),
         react(),
     ],
+    // Include font file extensions to avoid warnings
+    assetsInclude: ['**/*.otf', '**/*.woff', '**/*.woff2', '**/*.ttf'],
+    build: {
+        // Emit smaller chunks to prevent large file warnings
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return 'vendor'; // Split vendor files
+                    }
+                },
+            },
+        },
+        // File size limit for emitted chunks
+        chunkSizeWarningLimit: 500, // Increase the limit (default is 500 KB)
+    },
 });
