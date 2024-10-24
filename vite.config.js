eb2d3da890b3d404318cda4d704be52013132369
Mitul <mitulvpatel2211@gmail.com>
2024-10-24 21:50:31 +0530

    Vite config changes

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
@@ -10,20 +11,9 @@ export default defineConfig({
         }),
         react(),
     ],
-    // Include font file extensions to avoid warnings
-    assetsInclude: ['**/*.otf', '**/*.woff', '**/*.woff2', '**/*.ttf'],
-    build: {
-        // Emit smaller chunks to prevent large file warnings
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        return 'vendor'; // Split vendor files
-                    }
-                },
-            },
+    resolve: {
+        alias: {
+            $fonts: resolve(__dirname, 'static/fonts'), // Alias for fonts directory
         },
-        // File size limit for emitted chunks
-        chunkSizeWarningLimit: 500, // Increase the limit (default is 500 KB)
     },
 });
