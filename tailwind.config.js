e81d3be5acb22e7b5a7dd491e061b8177622144f
Mitul <mitulvpatel2211@gmail.com>
2024-12-10 21:46:32 +0530

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.jsx',
+        './resources/js/**/*.js', // Include JS files if used
+        './resources/js/**/*.tsx', // Include TSX files if used
+        './resources/js/**/*.ts', // Include TS files if used
         flowbite.content(),
     ],
 
@@ -22,7 +25,7 @@ export default {
                 greenStar: '#00b67a',
                 primary:'#FE753E',
                 secondary: '#5459E8',
-              },
+            },
         },
     },
 
